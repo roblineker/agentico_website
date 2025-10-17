b9b3c1499bae3ab69eca676db7f8bcc8fb91e876
roblineker <roblineker@gmail.com>
2025-10-17 20:29:21 +1000

    Update Next.js configuration and package scripts: set output to 'export', enable trailing slashes, and unoptimized images; modify build script for standard build process and add export script.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Configure the base path if your repository name is not the domain
+  // basePath: '/your-repo-name',
+  // assetPrefix: '/your-repo-name/',
 };
 
 export default nextConfig;
