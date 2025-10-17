0b2ff5fff46f9d5c9a56aafafe87244cc0f182dd
roblineker <roblineker@gmail.com>
2025-10-17 21:00:14 +1000

    Refactor Next.js configuration and package scripts: remove unused options, update build script for turbopack, and delete GitHub Actions workflow and .nojekyll file to streamline deployment process.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  // Configure the base path if your repository name is not the domain
-  // basePath: '/your-repo-name',
-  // assetPrefix: '/your-repo-name/',
+  /* config options here */
 };
 
 export default nextConfig;
